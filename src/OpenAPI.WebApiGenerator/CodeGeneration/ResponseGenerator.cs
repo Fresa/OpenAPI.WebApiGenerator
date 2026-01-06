5187da5c1412377153a27f356e9187341005fc95
Fredrik Arvidsson <fresa@fresa.se>
2026-01-06 11:37:19 +0100

    fix whitespace normalization for responses

--- src/OpenAPI.WebApiGenerator/CodeGeneration/ResponseGenerator.cs
+++ src/OpenAPI.WebApiGenerator/CodeGeneration/ResponseGenerator.cs
@@ -10,28 +10,28 @@ internal sealed class ResponseGenerator(List<ResponseContentGenerator> responseB
     public SourceCode GenerateResponseClass(string @namespace, string path)
     {
         return new SourceCode($"{path}/Response.g.cs",
-            $$"""
-                #nullable enable
-                using Corvus.Json;
-                using System.Text.Json;
-                using {{httpResponseExtensionsGenerator.Namespace}};
-              
-                namespace {{@namespace}};
-                
-                internal abstract partial class Response
-                {
-                    {{Enumerable.Range(1, 5).AggregateToString(i => 
-                        $$"""
-                          protected int Validate{{i}}xxStatusCode(int code) 
-                            => (code >= {{i}}00 && code <= {{i}}99) ? code : throw new InvalidOperationException($"Expected {{i}}xx status code, got {code}");
-                          """)}}
-                    
-                    internal abstract void WriteTo(HttpResponse httpResponse);
-                
-                    {{responseBodyGenerators.AggregateToString(generator => 
-                        generator.GenerateResponseContentClass())}}
-                }
-                #nullable restore
-              """);
+$$"""
+#nullable enable
+using Corvus.Json;
+using System.Text.Json;
+using {{httpResponseExtensionsGenerator.Namespace}};
+
+namespace {{@namespace}};
+
+internal abstract partial class Response
+{
+{{Enumerable.Range(1, 5).AggregateToString(i => 
+$$"""
+    protected int Validate{{i}}xxStatusCode(int code) 
+        => (code >= {{i}}00 && code <= {{i}}99) ? code : throw new InvalidOperationException($"Expected {{i}}xx status code, got {code}");
+""")}}
+    
+    internal abstract void WriteTo(HttpResponse httpResponse);
+
+    {{responseBodyGenerators.AggregateToString(generator => 
+        generator.GenerateResponseContentClass()).Indent(4)}}
+}
+#nullable restore
+""");
     }
 }
