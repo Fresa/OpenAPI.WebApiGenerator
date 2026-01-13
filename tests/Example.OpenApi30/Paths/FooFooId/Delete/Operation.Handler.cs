72e59ed11ee04a5c63ae58034c93ab34b8c3985a
Fredrik Arvidsson <fresa@fresa.se>
2026-01-13 23:38:20 +0100

    test(openapi30): add example api and integration tests

---
+++ tests/Example.OpenApi30/Paths/FooFooId/Delete/Operation.Handler.cs
@@ -0,0 +1,10 @@
+namespace Example.OpenApi30.Paths.FooFooId.Delete;
+
+internal partial class Operation
+{
+    internal partial Task<Response> HandleAsync(Request request, CancellationToken cancellationToken)
+    {
+        var response = new Response.OK200();
+        return Task.FromResult<Response>(response);
+    }
+}
