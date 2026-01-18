d29e6a339562b010cb956c7ee4ff09139ce69480
Fredrik Arvidsson <fresa@fresa.se>
2026-01-18 15:07:04 +0100

    feat: add support for yaml formatted OpenAPI specifications

--- src/OpenAPI.WebApiGenerator/CodeGeneration/SchemaGenerator.cs
+++ src/OpenAPI.WebApiGenerator/CodeGeneration/SchemaGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 using Corvus.Json;
 using Corvus.Json.CodeGeneration;
 using Corvus.Json.CodeGeneration.CSharp;
@@ -25,12 +26,11 @@ internal sealed class SchemaGenerator(
     private readonly HashSet<string> _fileCache = [];
 
     internal static SchemaGenerator For(
-        OpenApiSpecVersion openApiSpecVersion,
-        IDocumentResolver documentResolver,
+        OpenApiSpecification openApiSpecification, 
         string rootNamespace,
         SourceProductionContext context)
     {
-        var vocabulary = openApiSpecVersion switch
+        var vocabulary = openApiSpecification.Version switch
         {
             OpenApiSpecVersion.OpenApi2_0 =>
                 Corvus.Json.CodeGeneration.Draft4.VocabularyAnalyser.DefaultVocabulary,
@@ -40,8 +40,14 @@ internal sealed class SchemaGenerator(
                 Corvus.Json.CodeGeneration.Draft202012.VocabularyAnalyser.DefaultVocabulary,
             OpenApiSpecVersion.OpenApi3_2 =>
                 Corvus.Json.CodeGeneration.Draft202012.VocabularyAnalyser.DefaultVocabulary,
-            _ => throw new InvalidOperationException($"OpenAPI specification {openApiSpecVersion} is not supported")
+            _ => throw new InvalidOperationException($"OpenAPI specification {openApiSpecification.Version} is not supported")
         };
+        var documentResolver = new PrepopulatedDocumentResolver();
+        if (!documentResolver.AddDocument(openApiSpecification.Url, openApiSpecification.JsonDocument))
+        {
+            throw new InvalidOperationException("Could not add OpenApi document");
+        }
+
         var globalOptions =
             new SourceGeneratorHelpers.GlobalOptions(
                 fallbackVocabulary: vocabulary,
---
+++ src/OpenAPI.WebApiGenerator/OpenApi/OpenApiSpecifiction.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using Corvus.Json;
+using Microsoft.OpenApi;
+
+namespace OpenAPI.WebApiGenerator.OpenApi;
+
+internal sealed class OpenApiSpecification(
+    OpenApiDocument document,
+    OpenApiSpecVersion version,
+    JsonReference url,
+    JsonDocument jsonDocument)
+{
+    public OpenApiDocument Document { get; } = document;
+    public OpenApiSpecVersion Version { get; } = version;
+    public JsonReference Url { get; } = url;
+    public JsonDocument JsonDocument { get; } = jsonDocument;
+}
