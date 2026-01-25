3b65dd4f98ee9d3064b6c1802fcf0ea184871971
Fredrik Arvidsson <fresa@fresa.se>
2026-01-25 12:17:03 +0100

    test: add oauth2 implicit flow example

--- tests/Example.OpenApi31/Program.cs
+++ tests/Example.OpenApi31/Program.cs
@@ -1,6 +1,19 @@
 using Example.OpenApi31;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication()                                                          
+    .AddJwtBearer(SecuritySchemes.PetstoreAuthKey, options =>
+    {
+        var authority =
+            new Uri(SecuritySchemes.PetstoreAuth.Flows.Implicit.AuthorizationUrl).GetLeftPart(UriPartial.Authority);
+        options.Authority = authority;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = authority,                                                      
+            ValidAudience = authority,
+        };
+    }); 
 builder.AddOperations(builder.Configuration.Get<WebApiConfiguration>());
 var app = builder.Build();
 app.MapOperations();
