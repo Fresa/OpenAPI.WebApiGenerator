2f25a9363510decece173726309f0eea983a0fae
Fredrik Arvidsson <fresa@fresa.se>
2026-01-25 01:13:07 +0100

    test: add OIDC auth test handler

---
+++ OpenAPI.IntegrationTestHelpers/Observability/WebHostBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace OpenAPI.IntegrationTestHelpers.Observability;
+
+public static class WebHostBuilderExtensions
+{
+    public static IWebHostBuilder AddLogging(this IWebHostBuilder builder) =>
+        builder.ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddConsole();
+            logging.AddDebug();
+            logging.SetMinimumLevel(LogLevel.Trace);
+
+            logging.AddFilter("Microsoft.AspNetCore.Authentication", LogLevel.Trace);
+            logging.AddFilter("Microsoft.AspNetCore.Authorization", LogLevel.Trace);
+            logging.AddFilter("Microsoft.AspNetCore.Routing", LogLevel.Trace);
+        });
+}
--- tests/Example.OpenApi31.IntegrationTests/FooApplicationFactory.cs
+++ tests/Example.OpenApi31.IntegrationTests/FooApplicationFactory.cs
@@ -1,7 +1,34 @@
+using System.Net.Http.Headers;
 using JetBrains.Annotations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OpenAPI.IntegrationTestHelpers.Auth;
+using OpenAPI.IntegrationTestHelpers.Observability;
 
 namespace Example.OpenApi31.IntegrationTests;
 
 [UsedImplicitly]
-public class FooApplicationFactory : WebApplicationFactory<Program>;
+public class FooApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.Insert(0,
+                ServiceDescriptor.Singleton<IPostConfigureOptions<JwtBearerOptions>, HotWiredJwtBackchannelHandler>());
+        });
+
+        builder.AddLogging();
+    }
+    
+    protected override void ConfigureClient(HttpClient client)
+    {
+        base.ConfigureClient(client);
+        client.DefaultRequestHeaders.Authorization =
+            AuthenticationHeaderValue.Parse($"Bearer {OIDCAuthHttpHandler.Jwt}");
+    }
+}
