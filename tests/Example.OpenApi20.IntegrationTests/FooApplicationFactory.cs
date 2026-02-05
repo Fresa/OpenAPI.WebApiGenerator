2f4cfddd7f10e5d0c0f7d2d4c65518456240763f
Fredrik Arvidsson <fresa@fresa.se>
2026-02-05 18:52:48 +0100

    test(auth): include all security schemes and run 401 and 403 scenarios

--- tests/Example.OpenApi20.IntegrationTests/FooApplicationFactory.cs
+++ tests/Example.OpenApi20.IntegrationTests/FooApplicationFactory.cs
@@ -1,7 +1,22 @@
-﻿using JetBrains.Annotations;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using OpenAPI.IntegrationTestHelpers.Auth;
+using OpenAPI.IntegrationTestHelpers.Observability;
 
 namespace Example.OpenApi20.IntegrationTests;
 
 [UsedImplicitly]
-public class FooApplicationFactory : WebApplicationFactory<Program>;
+public class FooApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.InjectJwtBackChannelHandler();
+        });
+
+        builder.AddLogging();
+    }
+}
--- tests/Example.OpenApi20/Program.cs
+++ tests/Example.OpenApi20/Program.cs
@@ -1,6 +1,37 @@
+using Corvus.Json;
+using Example.OpenApi.Auth;
 using Example.OpenApi20;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication()
+    .AddJwtBearer(SecuritySchemes.PetstoreAuthKey, options =>
+    {
+        var authority =
+            new Uri(SecuritySchemes.PetstoreAuth.Flows.Implicit.AuthorizationUrl).GetLeftPart(UriPartial.Authority);
+        options.Authority = authority;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = authority,
+            ValidAudience = authority,
+        };
+    })
+    .AddScheme<ApiKeyAuthenticationOptions, ApiKeyAuthenticationHandler>(
+        SecuritySchemes.SecretKeyKey,
+        options =>
+        {
+            options.GetApiKey = context =>
+            {
+                var parameter = SecuritySchemes.SecretKey.GetParameter(context);
+                return parameter.Validate(ValidationContext.ValidContext).IsValid
+                    ? (true, parameter.GetString()!)
+                    : (false, null);
+            };
+        })
+    .AddScheme<BasicAuthenticationOptions, BasicAuthenticationHandler>(
+        SecuritySchemes.BasicAuthKey,
+        _ => { });
+
 builder.AddOperations(builder.Configuration.Get<WebApiConfiguration>());
 var app = builder.Build();
 app.MapOperations();
