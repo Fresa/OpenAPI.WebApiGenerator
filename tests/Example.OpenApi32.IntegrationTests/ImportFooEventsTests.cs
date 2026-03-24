0425f7bbe35bd9f565a51e32a7aba7ae45f48b73
Fredrik Arvidsson <fresa@fresa.se>
2026-03-24 23:59:45 +0100

    test(request): let last character always be a new line as supported by all sequential media types

--- tests/Example.OpenApi32.IntegrationTests/ImportFooEventsTests.cs
+++ tests/Example.OpenApi32.IntegrationTests/ImportFooEventsTests.cs
@@ -24,6 +24,7 @@ public class ImportFooEventsTests(FooApplicationFactory app) : FooTestSpecificat
                 $$"""
                 {{prefix}}{ "Name": "test" }
                 {{prefix}}{ "Name": "another test" }
+                
                 """, mediaType)
         }, CancellationToken);
         
