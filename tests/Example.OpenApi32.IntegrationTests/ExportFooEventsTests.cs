985fb9bf99b6805d89402bcc0ff6414daa8dac9a
Fredrik Arvidsson <fresa@fresa.se>
2026-03-25 00:02:13 +0100

    test(response): verify all supported sequential media types

--- tests/Example.OpenApi32.IntegrationTests/ExportFooEventsTests.cs
+++ tests/Example.OpenApi32.IntegrationTests/ExportFooEventsTests.cs
@@ -7,8 +7,13 @@ namespace Example.OpenApi32.IntegrationTests;
 
 public class ExportFooEventsTests(FooApplicationFactory app, ITestOutputHelper testOutput) : FooTestSpecification, IClassFixture<FooApplicationFactory>
 {
-    [Fact]
-    public async Task ExportingFooEvents_ShouldReturnOkWithJsonl()
+    [Theory]
+    [InlineData("application/jsonl")]
+    [InlineData("application/x-jsonlines")]
+    [InlineData("application/x-ndjson")]
+    [InlineData("application/json-seq")]
+    [InlineData("application/geo+json-seq")]
+    public async Task ExportingFooEvents_ShouldReturnOkWithSequentialJson(string mediaType)
     {
         using var client = app.CreateClient();
         var request = new HttpRequestMessage
@@ -16,37 +21,12 @@ public class ExportFooEventsTests(FooApplicationFactory app, ITestOutputHelper t
             RequestUri = new Uri(client.BaseAddress!, "/foo/1/events"),
             Method = HttpMethod.Get
         };
-        request.Headers.Accept.ParseAdd("application/jsonl");
+        request.Headers.Accept.ParseAdd(mediaType);
 
         var result = await client.SendAsync(request, CancellationToken);
 
         result.StatusCode.Should().Be(HttpStatusCode.OK);
-        result.Content.Headers.ContentType?.MediaType.Should().Be("application/jsonl");
-
-        var content = await result.Content.ReadAsStringAsync(CancellationToken);
-        testOutput.WriteLine("Content:");
-        testOutput.WriteLine(content);
-        var lines = content.Split('\n', StringSplitOptions.RemoveEmptyEntries);
-        lines.Should().HaveCount(2);
-        JsonNode.Parse(lines[0]).GetValue<string>("#/Name").Should().Be("foo1");
-        JsonNode.Parse(lines[1]).GetValue<string>("#/Name").Should().Be("foo2");
-    }
-    
-    [Fact]
-    public async Task ExportingFooEvents_ShouldReturnOkWithJsonSeq()
-    {
-        using var client = app.CreateClient();
-        var request = new HttpRequestMessage
-        {
-            RequestUri = new Uri(client.BaseAddress!, "/foo/1/events"),
-            Method = HttpMethod.Get
-        };
-        request.Headers.Accept.ParseAdd("application/json-seq");
-
-        var result = await client.SendAsync(request, CancellationToken);
-
-        result.StatusCode.Should().Be(HttpStatusCode.OK);
-        result.Content.Headers.ContentType?.MediaType.Should().Be("application/json-seq");
+        result.Content.Headers.ContentType?.MediaType.Should().Be(mediaType);
 
         var content = await result.Content.ReadAsStringAsync(CancellationToken);
         testOutput.WriteLine("Content:");
--- tests/Example.OpenApi32/Paths/FooFooIdEvents/Get/Operation.Handler.cs
+++ tests/Example.OpenApi32/Paths/FooFooIdEvents/Get/Operation.Handler.cs
@@ -1,3 +1,5 @@
+using Example.OpenApi32.Components.Schemas;
+
 namespace Example.OpenApi32.Paths.FooFooIdEvents.Get;
 
 internal partial class Operation
@@ -9,16 +11,33 @@ internal partial class Operation
             case false:
             case true when matchedMediaType == Response.OK200.ApplicationJsonl.ContentMediaType:
                 var jsonl = new Response.OK200.ApplicationJsonl(request);
-                jsonl.WriteItem(Components.Schemas.FooProperties.Create(name: "foo1"));
-                jsonl.WriteItem(Components.Schemas.FooProperties.Create(name: "foo2"));
+                WriteItems(jsonl.WriteItem);
                 return Task.FromResult<Response>(jsonl);
+            case true when matchedMediaType == Response.OK200.ApplicationXJsonlines.ContentMediaType:
+                var jsonLines = new Response.OK200.ApplicationXJsonlines(request);
+                WriteItems(jsonLines.WriteItem);
+                return Task.FromResult<Response>(jsonLines);
+            case true when matchedMediaType == Response.OK200.ApplicationXNdjson.ContentMediaType:
+                var ndJson = new Response.OK200.ApplicationXNdjson(request);
+                WriteItems(ndJson.WriteItem);
+                return Task.FromResult<Response>(ndJson);
             case true when matchedMediaType == Response.OK200.ApplicationJsonSeq.ContentMediaType:
                 var jsonSeq = new Response.OK200.ApplicationJsonSeq(request);
-                jsonSeq.WriteItem(Components.Schemas.FooProperties.Create(name: "foo1"));
-                jsonSeq.WriteItem(Components.Schemas.FooProperties.Create(name: "foo2"));
+                WriteItems(jsonSeq.WriteItem);
                 return Task.FromResult<Response>(jsonSeq);
+            case true when matchedMediaType == Response.OK200.ApplicationGeoJsonSeq.ContentMediaType:
+                var geoJsonSeq = new Response.OK200.ApplicationGeoJsonSeq(request);
+                WriteItems(geoJsonSeq.WriteItem);
+                return Task.FromResult<Response>(geoJsonSeq);
             default:
                 throw new NotImplementedException($"Content media type {matchedMediaType} has not been implemented");
+
+        }
+        
+        void WriteItems(Action<FooProperties> write)
+        {
+            write(FooProperties.Create(name: "foo1"));
+            write(FooProperties.Create(name: "foo2"));                    
         }
     }
 }
